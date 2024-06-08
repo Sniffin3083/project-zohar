5d1cfbe295e23cb7ba44ac2aa28714c1551a2ec2
Griff-Thaxter <sniffinisakuntt@gmail.com>
2024-06-08 12:21:14 -0600

    Set up Links to checklists

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
-
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -13,8 +11,24 @@ function App() {
         <p className="mt-8 text-xl">This a web app used to help people keep track of their progress in completing any of the 3 mainline Xenoblade Chronicles games</p>
         <p className="mt-10 text-lg">To get started click one of the games below to start the checklist process for that game</p>
 
-        <img src="./images/XenobladeOneBoxArt.jpg" />    
-        </div>
+        <div className="mt-12 w-screen h-28 flex flex-row justify-center items-center items-stretch">
+          <div className="">
+            <Link to={"xeno1"}>
+              <img className="w-52 hover:w-56" src={require("./images/XenobladeOneBoxArt.jpg")} /> 
+            </Link>
+          </div>
+          <div>
+            <Link to={"xeno2"}>
+              <img className="w-44 mx-32 hover:w-48" src={require("./images/XenobladeTwoBoxArt.jpg")} /> 
+            </Link>
+          </div>
+          <div>
+            <Link to={"xeno3"}>
+              <img className="w-44 hover:w-48" src={require("./images/XenobladeThreeBoxArt.jpg")} /> 
+            </Link>
+          </div>
+        </div>   
+      </div>
     </div>
   );
 }
