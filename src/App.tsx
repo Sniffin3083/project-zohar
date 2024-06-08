4bd525cadb2b16ffa59c2d478c618d8bb5ccd2ca
Griff-Thaxter <sniffinisakuntt@gmail.com>
2024-06-08 11:38:22 -0600

    Started Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 function App() {
   return (
     <div className="App">
-      
+     <div className="mt-8">
+        <h1 className="text-5xl font-bold">Welcome to Project Zohar</h1>
+        <p className="mt-8 text-xl">This a web app used to help people keep track of their progress in completing any of the 3 mainline Xenoblade Chronicles games</p>
+        <p className="mt-10 text-lg">To get started click one of the games below to start the checklist process for that game</p>
+
+        <img src="./images/XenobladeOneBoxArt.jpg" />    
+        </div>
     </div>
   );
 }
