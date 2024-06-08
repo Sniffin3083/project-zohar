bd8d3093fccedd81c73eb30a32934bfd00894ca9
Griff-Thaxter <sniffinisakuntt@gmail.com>
2024-06-08 11:38:38 -0600

    Set Up React Router

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { 
+  createBrowserRouter,
+  RouterProvider
+ } from 'react-router-dom';
+
+ import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
