87742bb98d2bde8755972478a756c514c783b369
Sniffin3083 <thaxterg12@gmail.com>
2024-06-17 10:58:52 -0600

    Created data file

---
+++ src/data/XCOneData.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        id: 1,
+        title: "testTitle",
+        name: "teatTitle2"
+    },
+    {
+        id: 2,
+        title: "testTitle2",
+        name: "testName2"
+    }
+]
