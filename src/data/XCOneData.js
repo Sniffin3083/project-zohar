167d4498d5948de3b8d2e891909849ff2632dc3e
Sniffin3083 <thaxterg12@gmail.com>
2024-08-17 13:13:50 -0600

    Added the first bit of data

--- src/data/XCOneData.js
+++ src/data/XCOneData.js
@@ -1,12 +1,47 @@
 export default [
     {
         id: 1,
-        title: "testTitle",
-        name: "teatTitle2"
+        name: "Mementos of a Lost Son",
+        giver: "Cheryl",
+        area: "Colony 9 (Commercial District) 06:00 - 15:00",
+        prerequisites: "Access to Tephra Cave",
+        timed: "No",
+        exclusive: "No"
     },
     {
         id: 2,
-        title: "testTitle2",
-        name: "testName2"
-    }
+        name: "The Key to a Long Life",
+        giver: "Dionysis",
+        area: "Colony 9 (Residential District) 06:00 - 13:00",
+        prerequisites: "None",
+        timed: "No",
+        exclusive: "No"
+    },
+    {
+        id: 3,
+        name: "A Curry Conundrum",
+        giver: "Giorgio",
+        area: "Colony 9 (Commercial District) 06:00 - 18:00",
+        prerequisites: "Colony 9 area ☆1 1/2",
+        timed: "No",
+        exclusive: "No"
+    },
+    {
+        id: 4,
+        name: "Biscuits for a Grandson",
+        giver: "Marcia",
+        area: "Colony 9 (Commercial District) 9:00 - 15:00",
+        prerequisites: "None",
+        timed: "No",
+        exclusive: "No"
+    },
+    {
+        id: 5,
+        name: "Birithday Shoes",
+        giver: "Kantz",
+        area: "Colony 9 (Military District) 6:00 - 15:00",
+        prerequisites: "Mechonis Core cleared quests: Education-Crazy Suzanna, A Young Caotain's Trust/A Young Captains Revival.\tColony 9 Area ☆4 1/2",
+        timed: "No",
+        exclusive: "No"
+    },
 ]
