09a7ce0e128f0f85907199e500aba71ec595aa5d
Sniffin3083 <thaxterg12@gmail.com>
2024-09-10 09:34:28 -0600

    Updated formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,33 @@ import './App.css';
 import { Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-     <div className="mt-8">
-        <h1 className="text-5xl font-bold">Welcome to Project Zohar</h1>
-        <p className="mt-8 text-xl">This a web app used to help people keep track of their progress in completing any of the 3 mainline Xenoblade Chronicles games</p>
-        <p className="mt-10 text-lg">To get started click one of the games below to start the checklist process for that game</p>
+    return (
+        <div className="App">
+            <div className="mt-8">
+                <h1 className="text-5xl font-bold">Welcome to Project Zohar</h1>
+                <p className="mt-8 text-xl">This a web app used to help people keep track of their progress in completing any of the 3 mainline Xenoblade Chronicles games</p>
+                <p className="mt-10 text-lg">To get started click one of the games below to start the checklist process for that game</p>
 
-        <div className="mt-12 w-screen h-28 flex flex-row justify-center items-center items-stretch">
-          <div className="">
-            <Link to={"/xeno1"}>
-              <img className="w-52 hover:w-56" src={require("./images/XenobladeOneBoxArt.jpg")} /> 
-            </Link>
-          </div>
-          <div>
-            <Link to={"/xeno2"}>
-              <img className="w-44 mx-32 hover:w-48" src={require("./images/XenobladeTwoBoxArt.jpg")} /> 
-            </Link>
-          </div>
-          <div>
-            <Link to={"/xeno3"}>
-              <img className="w-44 hover:w-48" src={require("./images/XenobladeThreeBoxArt.jpg")} /> 
-            </Link>
-          </div>
-        </div>   
-      </div>
-    </div>
-  );
+                <div className="mt-12 w-screen flex flex-row justify-center items-center items-stretch">
+                    <div className="">
+                        <Link to={"/xeno1"}>
+                        <img className="w-52 hover:w-56" src={require("./images/XenobladeOneBoxArt.jpg")} /> 
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to={"/xeno2"}>
+                        <img className="w-44 mx-32 hover:w-48" src={require("./images/XenobladeTwoBoxArt.jpg")} /> 
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to={"/xeno3"}>
+                        <img className="w-44 hover:w-48" src={require("./images/XenobladeThreeBoxArt.jpg")} /> 
+                        </Link>
+                    </div>
+                </div>   
+            </div>
+        </div>
+    );
 }
 
 export default App;
