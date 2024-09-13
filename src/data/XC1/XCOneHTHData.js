3634a2a8697d737c4dad8edecd5971303fab1557
Sniffin3083 <thaxterg12@gmail.com>
2024-09-13 14:37:17 -0600

    good chunk of data added

---
+++ src/data/XC1/XCOneHTHData.js
@@ -0,0 +1,220 @@
+export default [
+    {
+        title: "Sunrise in the Park",
+        area: "Colony 9 - Outlook Park",
+        characters: "Fiora, Shulk",
+        affinity: <img src="./affinities/affinity-yellow.webp" alt="Yellow" />,
+        prerequisites: "None",
+        uid: "hth_sunriseInThePark"
+    },
+    {
+        title: "Overlooking the Colony",
+        area: "Colony 9 - Agora Shore",
+        characters: "Reyn, Sharla",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "Reconstruction Started",
+        uid: "hth_overlookingTheColony"
+    },
+    {
+        title: "Enduring Friendship",
+        area: "Colony 9 - Tephra Cave Entrance",
+        characters: "Reyn, Shulk",
+        affinity: <img src="./affinites/affinity-green.webp" alt="Green" />,
+        prerequisites: "None",
+        uid: "hth_eduringFriendship"
+    },
+    {
+        title: "A Heropon's Perspective",
+        area: "Colony 9 - Mechon Wreckage Site",
+        characters: "Riki, Sharla",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "Prison Island events",
+        uid: "hth_aHeroponsPerspective"
+    },
+    {
+        title: "Ancient Wreckage",
+        area: "Colony 9 - Cylinder Hangar",
+        characters: "Melia, Reyn",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "High Entia Tomb events",
+        uid: "hth_ancientWreckage"
+    },
+    {
+        title: "Watching Over Them",
+        area: "Colony 9 - Residential District",
+        characters: "Dunban, Reyn",
+        affinity: <img src="./affinites/affinity-heart.webp" alt="Heart" />,
+        prerequisites: "Galahad Fortress events",
+        uid: "hth_watchingOverThem"
+    },
+    {
+        title: "Fiora's Cooking",
+        area: "Colony 9 - Dunban's House",
+        characters: "Fiora, Reyn",
+        affinity: <img src="./affinites/affinity-green.webp" alt="Green" />,
+        prerequisites: "Mechon raid",
+        uid: "hth_fiorasCooking"
+    },
+    {
+        title: "Glowing in the Night",
+        area: "Tephra Cave - Spring of Grief",
+        characters: "Dunban, Riki",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "None",
+        uid: "hth_glowingInTheNight"
+    },
+    {
+        title: "A Scene Revisited",
+        area: "Tephra Cave - Kneecap Hill",
+        characters: "Fiora, Reyn",
+        affinity: <img src="./affinites/affinity-heart.webp" alt="Heart" />,
+        prerequisites: "Mechon Raid",
+        uid: "hth_aSceneRevisited"
+    },
+    {
+        title: "The Legend of the Spider",
+        area: "Tephra Cave - Bone Corridor",
+        characters: "Reyn, Shulk",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "Mechonis Core events",
+        uid: "hth_theLegendOfTheSpider"
+    },
+    {
+        title: "What's On Reyn's Mind",
+        area: "Bionis' Leg - Inside Refugee Camp",
+        characters: "Reyn, Sharla",
+        affinity: <img src="./affinites/affinity-green.webp" alt="green" />,
+        prerequisites: "None",
+        uid: "hth_whatsOnReynsMind"
+    },
+    {
+        title: "Heir to the Monado",
+        area: "Bionis' Leg - Outside Refugee Camp",
+        characters: "Dunban, Reyn",
+        affinity: <img src="./affinites/affinity-green.webp" alt="Green" />,
+        prerequisites: "None",
+        uid: "hth_heirToTheMonado"
+    },
+    {
+        title: "Geography Lesson",
+        area: "Bionis' Leg - Believer's Paradise",
+        characters: "Dunban, Shulk",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "None",
+        uid: "hth_geographyLesson"
+    },
+    {
+        title: "Revisiting the Past",
+        area: "Bionis' Leg - Rho Oasis",
+        characters: "Dunban, Sharla",
+        affinity: <img src="./affinites/affinity-heart.webp" alt="Heart" />,
+        prerequisites: "None",
+        uid: "hth_revisitingThePast"
+    },
+    {
+        title: "What Visions May Bring",
+        area: "Bionis' Leg - Raguel Bridge North",
+        characters: "Sharla, Shulk",
+        affinity: <img src="./affinites/affinity-green.webp" alt="Green" />,
+        prerequisites: "Ether Mine events",
+        uid: "hth_whatVisionsMayBring"
+    },
+    {
+        title: "Strength of Heart",
+        area: "Colony 6 - West of Main Entrance",
+        characters: "Dunban, Shulk",
+        affinity: <img src="./affinites/affinity-heart.webp" alt="Heart" />,
+        prerequisites: "Nature Lv 3",
+        uid: "hth_strengthOfHeart"
+    },
+    {
+        title: "One Year On",
+        area: "Colony 6 - Pod Depot",
+        characters: "Dunban, Reyn",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "None",
+        uid: "hth_oneYearOn"
+    },
+    {
+        title: "Renewed Determination",
+        area: "Colony 6 - Freight Elevator",
+        characters: "Reyn, Shulk",
+        affinity: <img src="./affinites/affinity-heart.webp" alt="Heart" />,
+        prerequisites: "Mechonis Core events",
+        uid: "hth_renewedDetermination"
+    },
+    {
+        title: "Dunban's Right Arm",
+        area: "Colony 6 - Turret at Hope Farm",
+        characters: "Dunban, Sharla",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "Galahad Fortress events",
+        uid: "hth_dunbansRightArm"
+    },
+    {
+        title: "Recovery and Reflection",
+        area: "Colony 6 - Ridge at Hope Farm",
+        characters: "Fiora, Dunban",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "Agniratha - Central Tower events",
+        uid: "hth_recoveryAndReflection"
+    },
+    {
+        title: "The Colony Reborn",
+        area: "Colony 6 - Reconstruction HQ",
+        characters: "Sharla, Shulk",
+        affinity: <img src="./affinites/affinity-heart.webp" alt="Heart" />,
+        prerequisites: "All Lv 5",
+        uid: "hth_theColonyReborn"
+    },
+    {
+        title: "Quiet Time",
+        area: "Colony 6 - Park",
+        characters: "Fiora, Riki",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "Special Lv 3",
+        uid: "hth_quietTime"
+    },
+    {
+        title: "A Broken Watch",
+        area: "Ether Mine - Mining Base",
+        characters: "Sharla, Shulk",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "None",
+        uid: "hth_aBrokenWatch"
+    },
+    {
+        title: "A Wistful Glow",
+        area: "Ether Mine - Central Terminal",
+        characters: "Reyn, Sharla",
+        affinity: <img src="./affinites/affinity-heart.webp" alt="Heart" />,
+        prerequisites: "Agniratha - Meyneth Shrine events",
+        uid: "hth_aWistfulGlow"
+    },
+    {
+        title: "The Shimmering Marsh",
+        area: "Satorl Marsh - Zaldania Waterfall",
+        characters: "Dunban, Shulk",
+        affinity: <img src="./affinites/affinity-green.webp" alt="Green" />,
+        prerequisites: "None",
+        uid: "hth_theShimmeringMarsh"
+    },
+    {
+        title: "High Entia History",
+        area: "Satorl Marsh - Sororal Statues",
+        characters: "Dunban, Melia",
+        affinity: <img src="./affinites/affinity-cloud.webp" alt="Cloud" />,
+        prerequisites: "High Entia Tomb events",
+        uid: "hth_highEntiaHistory"
+    },
+]
+/*
+{
+    title: "",
+    area: "",
+    characters: "",
+    affinity: <img src="./affinites/affinity-.webp" alt="" />,
+    prerequisites: "",
+    uid: "hth_"
+},
+*/
