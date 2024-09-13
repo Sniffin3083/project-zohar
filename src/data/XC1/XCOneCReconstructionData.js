b869971ad4fb3a450dbf7b92753db7d75921d794
Sniffin3083 <thaxterg12@gmail.com>
2024-09-13 12:08:54 -0600

    Added all Colony6 Reconstruction Data

---
+++ src/data/XC1/XCOneCReconstructionData.js
@@ -0,0 +1,130 @@
+export default [
+    {
+        name: "Housing Level 1",
+        items: "x2 Spotted Volff Hide (Volff on Bionis' Leg) | x2 Steel Silk (Tephra Collectable) Peppino (☆3)",
+        funds: "9,000 G",
+        uid: "cReconstruction_housingLevel1"
+    },
+    {
+        name: "Housing Level 2",
+        items: "x4 Bunnia Scent Wood (Bunnia in Satorl Marsh) Bokoko at the Nopon Merchant Camp (☆1) | x1 Fossil Monkey (Makna Forest Collectable) Rasha Overtrade, Berryjammy (☆2), Puko (☆2)",
+        funds: "19,000 G",
+        uid: "cReconstruction_housingLevel2"
+    },
+    {
+        name: "Housing Level 3",
+        items: "x1 Eks Iron Heart (Eks in Makna Forest) Nelo (☆2) | x4 Sturdy Armor (Andos in Eryth Sea) Galdo and Jer'ell (☆1) | x2 Oil Branch (Eryth Collectable) Scarlen and Ricoth (☆2)",
+        funds: "55,000 G",
+        uid: "cReconstruction_housingLevel3"
+    },
+    {
+        name: "Housing Level 4",
+        items: "x5 Ponio Hoof Seal (Ponio on Fallen Arm) Eleqa (☆5) | x3 Royal Volff Hide (Volff on Fallen Arm) Neonik (overtrade), Xekit (☆5) | x3 Warning Lamp (Fallen arm Collectable) Natalia (overtrade), Karlos (☆5) | x2 Retro Diode (Mechonis Field Collectable) Norara (Colony 6 ☆5)",
+        funds: "120,000 G",
+        uid: "cReconstruction_housingLevel4"
+    },
+    {
+        name: "Housing Level 5",
+        items: "x2 Vang Star Wing (Vang in Windy Cave and Tephra Cave) | x3 Gogol Horn (Gogol on Bionis' Leg and Satorl Marsh) | x2 Red Frontier (Sword Valley Collectable) | x2 Black Styrene (Central Factory Collectable) Peppino (Colony 6 ☆5) | x3 Rainbow Zirconia (Colony 9 Collectable) Dionysis (☆1)",
+        funds: "300,000 G",
+        uid: "cReconstruction_housingLevel5"
+    },
+    {
+        name: "Commerce Level 1",
+        items: "x2 Igna Hide Jacket (Igna in Satorl Marsh and Exile Fortress) | x2 Amblygon Turtle (Collectable in Colony 6 Hope Farm) Arda (Colony 6 ☆1)",
+        funds: "10,000 G",
+        uid: "cReconstruction_commerceLevel1"
+    },
+    {
+        name: "Commerce Level 2",
+        items: "x3 Hode Plank (Hyle Hode in Makna Forest) Lupa (☆3) | x2 Ready Coil (Ether Mine Collectable)  Olga (Colony 6 ☆1)",
+        funds: "22,000 G",
+        uid: "cReconstruction_commerceLevel2"
+    },
+    {
+        name: "Commerce Level 3",
+        items: "x3 Shiny Kromar Hide (Kromar in Eryth Sea) Arielle (☆2) | x2 Slick Kromar Stone (Kromar in Eryth Sea) Yura (Alcamoth ☆2) | x2 Blue Ladybird (High Entia Tomb Collectable) Cian (Alcamoth ☆4)",
+        funds: "55,000 G",
+        uid: "cReconstruction_commerceLevel3"
+    },
+    {
+        name: "Commerce Level 4",
+        items: "x5 Piranhax Fishmeal (Fair Piranhax in Fallen Arm) | x8 Silver Antol Fibre (Antol on the Fallen Arm, Digit 1) Shilx (Hidden Village ☆3) | x3 Sour Turnip (Collectable on the Fallen Arm) Eleqa (☆3), Voltak (overtrade) | x2 Mossy Panel (Collectable in Mechonis Field) Nopo'rikh (☆5)",
+        funds: "100,000 G",
+        uid: "cReconstruction_commerceLevel4"
+    },
+    {
+        name: "Commerce Level 5",
+        items: "x1 Ocean Elixer of Life (Kyel Lexos' in Colony 9 after Mechonis Core) Atael after Mechonis Core (☆5) | x4 Ancient Sardi Meat (Throne Sardi in Colony 9) | x2 Art Core Coil (Galahad Fortress Collectable) Trade Kazat (☆4) | x1 Fortune Feather (Agniratha Collectable) | x4 Hill Firefly (Bionis' Leg Collectable)",
+        funds: "250,000 G",
+        uid: "cReconstruction_commerceLevel5"
+    },
+    {
+        name: "Nature Level 1",
+        items: "x2 Sharp Hox Spur (Hox in Bionis' Leg) Matryona (Colony 6 ☆1) | x2 Dark Grape (Collectable: Tephra Cave) Anna (Bionis' Leg ☆1), Dorothy (☆3)",
+        funds: "5,000 G",
+        uid: "cReconstruction_natureLevel1"
+    },
+    {
+        name: "Nature Level 2",
+        items: "x3 Quadwing Bag (Quadwing in Satorl Marsh) | x3 Empress Beetle (Collectable: Frontier Village) Dobadoba (☆4)",
+        funds: "18,000 G",
+        uid: "cReconstruction_natureLevel2"
+    },
+    {
+        name: "Nature Level 3",
+        items: "x3 Jagged Tail (Eryth Hiln, Ansel in Eryth Sea) Kurralth (Alcamoth ☆1) Talia after Mechonis Core | x2 Despair Clover (Collectable: Eryth Sea) Atael (Alcamoth ☆2) | x2 Ice Cabbage (Collectable: Valak Mountain) Yura (Valak Mountain ☆4)",
+        funds: "40,000 G",
+        uid: "cReconstruction_natureLevel3"
+    },
+    {
+        name: "Nature Level 4",
+        items: "x3 Caterpile Silk (Royal Caterpile on Bionis' Leg) Sylviane (overtrade), Dorothy (☆5) | x3 Hox Daylight Spur (Dark Hox, White Hox in Bionis' Leg) Bozatrox (Mechonis Field ☆4), Jer'ell (overtrade) | x3 Oil Oyster (Collectable: Fallen Arm) Shilx (Fallen Arm ☆3), Qofaria (overtrade) | x3 White Plum (Collectable: Fallen Arm) Prox (☆3), Eleqa (overtrade)",
+        funds: "99,000 G",
+        uid: "cReconstruction_natureLevel4"
+    },
+    {
+        name: "Nature Level 5",
+        items: "x3 Ardun Elder Beard (Magnis Ardun in Bionis' Leg) | x1 Tokilos King Egg (Leg Tokilos in Bionis' Leg) | x2 Lewisia Silver (Collectable: Agniratha) Berryjammy (Colony 6 ☆5) | x2 Black Liver Bean (Collectable: Bionis' Interior) | x2 Black Beetle (Collectable: Ether Mine) Norara (Colony 6 ☆1)",
+        funds: "280,000 G",
+        uid: "cReconstruction_natureLevel5"
+    },
+    {
+        name: "Special Level 1",
+        items: "x1 Light Rain Element (Aqua Nebula on Bionis' Leg) Satata (Refugee Camp, overtrade) | x1 Kneecap Rock (Collectable: Tephra Cave) Werner (Colony 9 ☆2)",
+        funds: "8,000 G",
+        uid: "cReconstruction_specialLevel1"
+    },
+    {
+        name: "Special Level 2",
+        items: "x2 Dust Element (Caris Nebula in Makna Forest) Hoko (Frontier Village ☆4) | x3 Sea Berry (Collectable: Eryth Sea) Talia (Alcamoth ☆1",
+        funds: "25,000 G",
+        uid: "cReconstruction_specialLevel2"
+    },
+    {
+        name: "Special Level 3",
+        items: "x2 Squall Element (Bono Nebula in Eryth Sea) Nelo (Alcamoth ☆3) En Argentis after Mechonis Core | x2 Snow Element (Reef Nebula in Valak Mountain) Yura (Valak, overtrade) | x3 Lemonade Sky (Collectable: Makna Forest) Kilaki, Rono (☆3)",
+        funds: "50,000 G",
+        uid: "cReconstruction_specialLevel3"
+    },
+    {
+        name: "Special Level 4",
+        items: "x3 Flexible Selua Cell (Largo Selua in Bionis' Interior) | x3 Steel Selua Cell (Wool Selua in Bionis' Interior) | x2 Rainbow Slug (Collectable: Fallen Arm) | x2 Azure Hollyhock (Collectable: Mechonis Field) Scarlen after Mechonis Core",
+        funds: "130,000 G",
+        uid: "cReconstruction_specialLevel4"
+    },
+    {
+        name: "Special Level 5",
+        items: "x2 Inferno Element (Cratere Nebula in Prison Island) | x2 Bolt Element (Solare Nebula in Prison Island) | x3 Blue Light Amp (Collectable: Agniratha) Oleksiy (Colony 6 ☆5) | x2 Angel Engine X (Collectable: Central Factory) Shilx (Colony 6 ☆5) | x3 Rabbit Diode (Collectable: Colony 9) Moritz (☆1)",
+        funds: "400,000 G",
+        uid: "cReconstruction_specialLevel5"
+    },
+]
+/*
+{
+    name: "",
+    items: "",
+    funds: "",
+    uid: "cReconstruction_"
+},
+*/
