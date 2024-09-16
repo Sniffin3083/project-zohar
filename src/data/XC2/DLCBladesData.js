ddc0bd8d8cc31106d23a9b2816c382ac79e916a0
Sniffin3083 <thaxterg12@gmail.com>
2024-09-16 12:04:18 -0600

    Added Setup For Data

--- src/data/XC2/DLCBladesData.js
+++ src/data/XC2/DLCBladesData.js
@@ -1,8 +1,5 @@
-import NavBar from "./Navbar";
-import DLCBladesData from "../../data/XC2/DLCBladesData"
-
-export default function XCTwoDLCBlades() {
-    return (
-        <h1>Xenobalde 2</h1>
-    );
-}
+export default [
+    {
+        
+    }
+]
