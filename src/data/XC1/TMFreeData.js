8a33dee581124e4c29dff454539c76ba3929a494
Sniffin3083 <thaxterg12@gmail.com>
2024-09-15 19:50:23 -0600

    Data Added for Time Attack

---
+++ src/data/XC1/TMFreeData.js
@@ -0,0 +1,72 @@
+export default [
+    {
+        name: "Humble Beginnings",
+        prerequisites: "None",
+        waves: 3,
+        level: 20,
+        uid: "tmFree_humbleBeginnings"
+    },
+    {
+        name: "Fair Game",
+        prerequisites: "None",
+        waves: 3,
+        level: 25,
+        uid: "tmFree_fairGame"
+    },
+    {
+        name: "The Walls Have Eyes",
+        prerequisites: "After completing the events of the Ether Mine",
+        waves: 3,
+        level: 35,
+        uid: "tmFree_theWallsHaveEyes"
+    },
+    {
+        name: "Shaggy-Dog Story",
+        prerequisites: "Sometime after reaching Frontier Village and before reaching Eryth Sea",
+        waves: 2,
+        level: 35,
+        uid: "tmFree_shaggyDogStory"
+    },
+    {
+        name: "Oh, Brother!",
+        prerequisites: "After completing the events at the High Entia Tomb",
+        waves: 3,
+        level: 45,
+        uid: "tmFree_ohBrother"
+    },
+    {
+        name: "Third Wheel",
+        prerequisites: "After completing Fiora's Treatment",
+        waves: 3,
+        level: 55,
+        uid: "tmFree_thirdWheel"
+    },
+    {
+        name: "Sparks Fly, You Die",
+        prerequisites: "After reaching the Central Factory",
+        waves: 2,
+        level: 65,
+        uid: "tmFree_sparksFlyYouDie"
+    },
+    {
+        name: "Lost in a Dark Dream",
+        prerequisites: "After clearing the main story of Future Connected",
+        waves: 1,
+        level: 80,
+        uid: "tmFree_lostInADarkDream"
+    },
+    {
+        name: "He of the Metal Face",
+        prerequisites: "After reaching Bionis' Interior for the second time",
+        waves: 2,
+        level: 90,
+        uid: "tmFree_heOfTheMetalFace"
+    },
+    {
+        name: "Uniquely Disadvantaged",
+        prerequisites: "After reaching Prison Island for the second time",
+        waves: 3,
+        level: 99,
+        uid: "tmFree_uniquelyDisadvantaged"
+    },
+]
---
+++ src/data/XC1/TMRestrictedData.js
@@ -0,0 +1,97 @@
+import onePepper from "./timeAttackPeppers/1_pepper.webp"
+import twoPepper from "./timeAttackPeppers/2_pepper.webp"
+import threePepper from "./timeAttackPeppers/3_pepper.webp"
+import fourPepper from "./timeAttackPeppers/4_pepper.webp"
+
+export default [
+    {
+        name: "Humble Beginnings",
+        prerequisites: "None",
+        waves: 3,
+        difficulty: <img src={onePepper} alt="1 Pepper" />,
+        formation: "Reyn, Shulk",
+        uid: "tmRestricted_humbleBeginnings"
+    },
+    {
+        name: "Fair Game",
+        prerequisites: "None",
+        waves: 3,
+        difficulty: <img src={onePepper} alt="1 Pepper" />,
+        formation: "Sharla, Shulk, Reyn",
+        uid: "tmRestricted_fairGame"
+    },
+    {
+        name: "The Walls Have Eyes",
+        prerequisites: "After completing the events of the Ether Mine",
+        waves: 3,
+        difficulty: <img src={onePepper} alt="1 Pepper" />,
+        formation: "Dunban, Reyn, Sharla",
+        uid: "tmRestricted_theWallsHaveEyes"
+    },
+    {
+        name: "Shaggy-Dog Story",
+        prerequisites: "Sometime after reaching Frontier Village and before reaching Eryth Sea",
+        waves: 2,
+        difficulty: <img src={twoPepper} alt="2 Peppers" />,
+        formation: "Riki, Sharla, Shulk",
+        uid: "tmRestricted_shaggyDogStory"
+    },
+    {
+        name: "Oh, Brother!",
+        prerequisites: "After completing the events at the High Entia Tomb",
+        waves: 3,
+        difficulty: <img src={twoPepper} alt="2 Peppers" />,
+        formation: "Melia, Dunban, Riki",
+        uid: "tmRestricted_ohBrother"
+    },
+    {
+        name: "Third Wheel",
+        prerequisites: "After completing Fiora's Treatment",
+        waves: 3,
+        difficulty: <img src={twoPepper} alt="2 Peppers" />,
+        formation: "Fiora, Melia, Dunban",
+        uid: "tmRestricted_thirdWheel"
+    },
+    {
+        name: "Sparks Fly, You Die",
+        prerequisites: "After reaching the Central Factory",
+        waves: 2,
+        difficulty: <img src={threePepper} alt="3 Peppers" />,
+        formation: "Shulk, Reyn, Fiora",
+        uid: "tmRestricted_sparksFlyYouDie"
+    },
+    {
+        name: "Lost in a Dark Dream",
+        prerequisites: "After clearing the main story of Future Connected",
+        waves: 1,
+        difficulty: <img src={threePepper} alt="3 Peppers" />,
+        formation: "Melia, Riki, Fiora",
+        uid: "tmRestricted_lostInADarkDream"
+    },
+    {
+        name: "He of the Metal Face",
+        prerequisites: "After reaching Bionis' Interior for the second time",
+        waves: 2,
+        difficulty: <img src={threePepper} alt="3 Peppers" />,
+        formation: "Dunban, Sharla, Riki",
+        uid: "tmRestricted_heOfTheMetalFace"
+    },
+    {
+        name: "Uniquely Disadvantaged",
+        prerequisites: "After reaching Prison Island for the second time",
+        waves: 3,
+        difficulty: <img src={fourPepper} alt="4 Pepper" />,
+        formation: "Fiora, Melia, Sharla",
+        uid: "tmRestricted_uniquelyDisadvantaged"
+    },
+]
+/*
+{
+    name: "",
+    prerequisites: "",
+    waves: ,
+    difficulty: <img src={} alt="" />,
+    formation: "",
+    uid: "tmRestricted_"
+},
+*/
