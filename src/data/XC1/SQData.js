8520fa094ad4a5efd3dc9e1fe9b2c0a958c1bbbc
Sniffin3083 <thaxterg12@gmail.com>
2024-09-16 18:55:16 -0600

    Added all story quests

---
+++ src/data/XC1/SQData.js
@@ -0,0 +1,281 @@
+export default [
+    {
+        name: "Delivering Food",
+        giver: "Story Quest",
+        area: "Colony 9 (Dunban's House) Automatic",
+        prerequisites: "None",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_deliveringFood"
+    },
+    {
+        name: "The Ancient Ceremony",
+        giver: "Nopon Merchant",
+        area: "Satorl Marsh (Sororal Statues) Anytime",
+        prerequisites: "Sororal Statues reached",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_ancientCeremony"
+    },
+    {
+        name: "Ancient Ceremony Offerings",
+        giver: "Nopon Merchant",
+        area: "Satorl Marsh (Sororal Statues) Anytime",
+        prerequisites: "Sororal Statues reached | The Ancient Ceremony accepted",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_ancientCeremonyOfferings"
+    },
+    {
+        name: "Mystery Girl Rescue",
+        giver: "Story Quest",
+        area: "Makna Forest (Contaminated Area) Automatic",
+        prerequisites: "Makna Forest area reached",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_mysteryGirlRescue"
+    },
+    {
+        name: "An Errand for the Heropon",
+        giver: "Story Quest",
+        area: "Frontier Village Automatic",
+        prerequisites: "Frontier Village area reached",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_errandForTheHeropon"
+    },
+    {
+        name: "Save the Worker",
+        giver: "Story Quest",
+        area: "Alcamoth (Whitewing Palace) Automatic",
+        prerequisites: "Alcamoth reached.",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_saveWorker"
+    },
+    {
+        name: "Path to Prison Island",
+        giver: "Story Quest",
+        area: "Eryth Sea Automatic",
+        prerequisites: "In the course to Prison Island",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_pathToPrisonIsland"
+    },
+    {
+        name: "Sister Seals",
+        giver: "Story Quest",
+        area: "Eryth Sea Automatic",
+        prerequisites: "In the course to Prison Island | Path to Prison Island accepted",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_sisterSeals"
+    },
+    {
+        name: "The Magma Rock",
+        giver: "Story Quest",
+        area: "Valak Mountain (Sealed Tower)",
+        prerequisites: "Automatic",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_magmaRock"
+    },
+    {
+        name: "Lift Battle",
+        giver: "Story Quest",
+        area: "Galahad Fortress (Main Maintenance Bay) Automatic",
+        prerequisites: "None",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_liftBattle"
+    },
+    {
+        name: "Supply Station Battle",
+        giver: "Story Quest",
+        area: "Galahad Fortress (Main Control Room) Automatic",
+        prerequisites: "None",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_supplyStationBattle"
+    },
+    {
+        name: "Turbine Battle",
+        giver: "Story Quest",
+        area: "Galahad Fortress (Main Control Room) Automatic",
+        prerequisites: "Supply Station Battle accepted",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_turbineBattle"
+    },
+    {
+        name: "Fiora's Conviction",
+        giver: "Story Quest",
+        area: "Galahad Fortress (Face Maintenance Bay) Automatic",
+        prerequisites: "None",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_fiorasConviction"
+    },
+    {
+        name: "Chase Dickson",
+        giver: "Story Quest",
+        area: "Prison Island (Behemoth Nest) Automatic",
+        prerequisites: "Prison Island area reached for the second time",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_chaseDickson"
+    },
+    {
+        name: "Path to the Top",
+        giver: "Story Quest",
+        area: "Prison Island (Corridor of Silence) Automatic",
+        prerequisites: "Prison Island area reached for the second time | Chase Dickson complete",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_pathToTheTop"
+    },
+    {
+        name: "Reunion with Fiora",
+        giver: "Story Quest",
+        area: "Automatic",
+        prerequisites: "Fallen Arm area reached",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_reuinionWithFiora"
+    },
+    {
+        name: "We Made It",
+        giver: "Story Quest",
+        area: "Automatic",
+        prerequisites: "Fallen Arm area reached | Reunion with Fiora complete",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_weMadeIt"
+    },
+    {
+        name: "Fiora's Treatment",
+        giver: "Linada",
+        area: "Automatic",
+        prerequisites: "Miqol met in the Hidden Machina Village",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_fiorasTreatment"
+    },
+    {
+        name: "Aiming for the Top",
+        giver: "Story Quest",
+        area: "Mechonis Field (2nd Lift - GF) Automatic",
+        prerequisites: "Mechonis Field area reached",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_aimingForTheTop"
+    },
+    {
+        name: "Get the 3rd Lift Moving",
+        giver: "Story Quest",
+        area: "Mechonis Field (2nd Lift - 1F) Automatic",
+        prerequisites: "Mechonis Field area reached | Aiming for the Top complete",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_getThe3rdLiftMoving"
+    },
+    {
+        name: "Opening the Bulkhead",
+        giver: "Story Quest",
+        area: "Mechonis Field (3rd Lift - 3F) Automatic",
+        prerequisites: "Mechonis Field area reached | Get the 3rd Lift Moving complete",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_openingTheBulkhead"
+    },
+    {
+        name: "The High-Velocity Lift",
+        giver: "Story Quest",
+        area: "Mechonis Field (Upper Bulkhead Bridge) Automatic",
+        prerequisites: "Mechonis Field area reached | Opening the Bulkhead complete",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_highVelocityLift"
+    },
+    {
+        name: "Need Power!",
+        giver: "Story Quest",
+        area: "Mechonis Field (Upper Bulkhead Bridge) Automatic",
+        prerequisites: "Mechonis Field area reached | Opening the Bulkhead complete | The High-Velocity Lift accepted",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_needPower"
+    },
+    {
+        name: "To the Central Tower",
+        giver: "Story Quest",
+        area: "Central Factory (Central Gate) Automatic",
+        prerequisites: "Central Factory area reached",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_toTheCentralTower"
+    },
+    {
+        name: "Maintenance Wing Escape",
+        giver: "Story Quest",
+        area: "Central Factory (Maintenance Wing) Automatic",
+        prerequisites: "Central Factory area reached | To the Central Tower complete",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_maintenanceWingEscape"
+    },
+    {
+        name: "Find a Path to the Top",
+        giver: "Story Quest",
+        area: "Central Factory (Central Warehouse Lift) Automatic",
+        prerequisites: "Central Factory area reached | Maintenance Wing Escape complete",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_findAPathToTheTop"
+    },
+    {
+        name: "Materials for a Bomb",
+        giver: "Story Quest",
+        area: "Central Factory (Factory Vent) Automatic",
+        prerequisites: "Central Factory area reached | Maintenance Wing Escape complete | Find a Path to the Top accepted",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_materialsForABomb"
+    },
+    {
+        name: "The Central Tower Barrier",
+        giver: "Story Quest",
+        area: "Central Factory (Tower Boarding Gate) Automatic",
+        prerequisites: "",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_theCentralTowerBarrier"
+    },
+    {
+        name: "Shrine Transport",
+        giver: "Story Quest",
+        area: "Automatic",
+        prerequisites: "None",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_shrineTransport"
+    },
+    {
+        name: "Pillar Verification Devices",
+        giver: "Story Quest",
+        area: "Automatic",
+        prerequisites: "None",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_pillarVerificationDevices"
+    },
+    {
+        name: "Save Bionis",
+        giver: "Story Quest",
+        area: "Automatic",
+        prerequisites: "Mechonis Core reached",
+        timed: "No",
+        exclusive: "No",
+        uid: "q_saveBionis"
+    },
+]
