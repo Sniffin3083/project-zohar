410349840405f390bc93931b5bea39f2df6dbb78
Sniffin3083 <thaxterg12@gmail.com>
2024-09-16 19:13:39 -0600

    Renamed and added data

--- src/data/XC2/SQuestsData.js
+++
@@ -1,9 +0,0 @@
-export default [
-    {
-        name: "Cargo Escort",
-        area: "Argentum Trade Guild (Central Exchange)",
-        giver: "Ysolde",
-        prerequisites: "None",
-        uid: "sq_cargoEscort"
-    },
-]
---
+++ src/data/XC2/SideQuestsData.js
@@ -0,0 +1,75 @@
+export default [
+    {
+        name: "Cargo Escort",
+        area: "Argentum Trade Guild (Central Exchange)",
+        giver: "Ysolde",
+        prerequisites: "Big Job Preparations Story Quest complete",
+        uid: "sq_cargoEscort"
+    },
+    {
+        name: "Hide and Seek",
+        area: "Argentum Trade Guild (Bower Lounge)",
+        giver: "Aft",
+        prerequisites: "Big Job Preparations Story Quest complete",
+        uid: "sq_hideAndSeek"
+    },
+    {
+        name: "Better Late Than Never",
+        area: "Argentum Trade Guild (Bower Lounge)",
+        giver: "Gegebi",
+        prerequisites: "Rest at the Lemour Inn Once",
+        uid: "sq_betterLateThanNever"
+    },
+    {
+        name: "Into the Open Sky",
+        area: "Argentum Trade Guild (Goldmouth Flight Deck)",
+        giver: "Panpan",
+        prerequisites: "Chapter 3 started",
+        uid: "sq_intoTheOpenSky"
+    },
+    {
+        name: "Fame and Family",
+        area: "Argentum Trade Guild (Lemour Inn)",
+        giver: "Falala",
+        prerequisites: "Chapter 4 started | After Returning to Argentum Trade Guild",
+        uid: "sq_dameAndFamily"
+    },
+    {
+        name: "Bana's Secret Treasure",
+        area: "Argentum Trade Guild (Bana's Bedroom)",
+        giver: "Niranira",
+        prerequisites: "None",
+        uid: "sq_banasSecretTreasure"
+    },
+    {
+        name: "Uncover the Truth",
+        area: "Argentum Trade Guild (Central Exchange)",
+        giver: "Notice Board",
+        prerequisites: "None",
+        uid: "sq_uncoverTheTruth"
+    },
+    {
+        name: "Nopon Doubloons Ahoy",
+        area: "Argentum Trade Guild (Goldmouth Viewpoint)",
+        giver: "Nunui",
+        prerequisites: "None",
+        uid: "sq_noponDoubloonsAhoy"
+    },
+    {
+        name: "Bana's Revenge",
+        area: "Argentum Trade Guild (Chairman's Room)",
+        giver: "Niranira",
+        prerequisites: "Chapter 10 | Bana's Secret Treasure, Insurgent Investigations, Farewell, Good Friend complete",
+        uid: "sq_"
+    },
+]
+
+/*
+{
+    name: "",
+    area: "Argentum Trade Guild ()",
+    giver: "",
+    prerequisites: "",
+    uid: "sq_"
+},
+*/
