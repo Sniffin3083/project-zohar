666ba91f12b57d5e9571e88077f770ad3f4185b7
Griffin Thaxter <thaxterg12@gmail.com>
2024-09-21 21:25:55 -0600

    Half of the UM data added

--- src/data/XC2/UMonsterData.js
+++ src/data/XC2/UMonsterData.js
@@ -1,5 +1,323 @@
 export default [
     {
-        
-    }
+        name: "Moonlighting Elwyn",
+        level: 8,
+        area: "Gormott Province (Plains of Evening Calm)",
+        time: "Anytime",
+        uid: "um_moonlightElwyn"
+    },
+    {
+        name: "Sad Bernard",
+        level: 12,
+        area: "Gormott Province (Garanti Plain)",
+        time: "Anytime",
+        uid: "um_sadBernard"
+    },
+    {
+        name: "Remorseful Vaclav",
+        level: 14,
+        area: "Gormott Province (Solitary Block #1)",
+        time: "Anytime",
+        uid: "um_remorsefulVaclav"
+    },
+    {
+        name: "Sniping Brent",
+        level: 18,
+        area: "Gormott Province (Melnath's Shoulder)",
+        time: "Clear/Overcast",
+        uid: "um_snipingBrent"
+    },
+    {
+        name: "Supercharged Alfonso",
+        level: 20,
+        area: "Gormott Province (Masrah Spring)",
+        time: "Low Tide",
+        uid: "um_superchargedAlfonso"
+    },
+    {
+        name: "Implacable Dylan",
+        level: 23,
+        area: "Kingdom of Uraya (Acid Gullet)",
+        time: "Anytime",
+        uid: "um_implacableDylan"
+    },
+    {
+        name: "Martial Kamron",
+        level: 23,
+        area: "Kingdom of Uraya (Stone Gate Ruins)",
+        time: "Anytime",
+        uid: "um_martialKamron"
+    },
+    {
+        name: "Cunning Saggie",
+        level: 24,
+        area: "Kingdom of Uraya (Minnet Terrace)",
+        time: "Anytime",
+        uid: "um_cunningSaggie"
+    },
+    {
+        name: "Acute Melvyn",
+        level: 25,
+        area: "Gormott Province (Boulderbore Gate)",
+        time: "Anytime",
+        uid: "um_acuteMelvyn"
+    },
+    {
+        name: "Walker Trap",
+        level: 26,
+        area: "Kingdom of Uraya (Farlaine Wells)",
+        time: "Anytime",
+        uid: "um_walkerTrap"
+    },
+    {
+        name: "Nitpicking Beru",
+        level: 27,
+        area: "Gormott Province (Solitary Block #2)",
+        time: "Anytime",
+        uid: "um_nitpickingBeru"
+    },
+    {
+        name: "Anguished Solomon",
+        level: 28,
+        area: "Gormott Province (Coolley Lake)",
+        time: "Rain/thunderstorm",
+        uid: "um_anguishedSolomon"
+    },
+    {
+        name: "Demon-Shell Jacob",
+        level: 30,
+        area: "Empire of Mor Ardain (Mine Elevator)",
+        time: "Clear, Overcast, Sandstorm",
+        uid: "um_demonShellJacob"
+    },
+    {
+        name: "Man-Eating Glenn",
+        level: 32,
+        area: "Empire of Mor Ardain (Jelidorend Highlands)",
+        time: "Anytime",
+        uid: "um_manEatingGlenn"
+    },
+    {
+        name: "Decapitator Marvin",
+        level: 33,
+        area: "Empire of Mor Ardain (Old City of Teddim)",
+        time: "Anytime",
+        uid: "um_decapitatorMarvin"
+    },
+    {
+        name: "Judicial Kollin",
+        level: 33,
+        area: "Empire of Mor Ardain (Delizé's Infirmary)",
+        time: "Anytime",
+        uid: "um_judicialKollin"
+    },
+    {
+        name: "Myrmidon Eugene",
+        level: 33,
+        area: "Empire of Mor Ardain (Eggle Jetty (Abandoned))",
+        time: "Anytime",
+        uid: "um_myrmidonEugene"
+    },
+    {
+        name: "Confiscator Jimmy",
+        level: 38,
+        area: "Leftherian Archipelago (Daram Isle)",
+        time: "Anytime",
+        uid: "um_confiscatorJimmy"
+    },
+    {
+        name: "Jadeite Polly",
+        level: 38,
+        area: "Leftherian Archipelago (Baldotas Isle)",
+        time: "Aurora",
+        uid: "um_jadeitePolly"
+    },
+    {
+        name: "Skyfist Remington",
+        level: 39,
+        area: "Leftherian Archipelago (Ysheva Harbor)",
+        time: "Anytime",
+        uid: "um_skyfistRemington"
+    },
+    {
+        name: "Slasher Buffon",
+        level: 40,
+        area: "Empire of Mor Ardain (Mine No. 2 Entrance)",
+        time: "Clear, Overcast",
+        uid: "um_slasherBuffon"
+    },
+    {
+        name: "Azure Reginald",
+        level: 41,
+        area: "Kingdom of Uraya (Coralline Sanctum)",
+        time: "Clear/Overcast",
+        uid: "um_azureReginald"
+    },
+    {
+        name: "Deep-Green Oscar",
+        level: 41,
+        area: "Kingdom of Tantal (Sacred Ice Caverns)",
+        time: "Nighttime",
+        uid: "um_deepGreenOscar"
+    },
+    {
+        name: "Blue-Eyed Korbin",
+        level: 42,
+        area: "Kingdom of Tantal (Amphoret Pillar)",
+        time: "Snow",
+        uid: "um_blueEyedKorbin"
+    },
+    {
+        name: "Enlightened Morris",
+        level: 42,
+        area: "Kingdom of Tantal (Pippito's Haunt)",
+        time: "Anytime",
+        uid: "um_enlightedMorris"
+    },
+    {
+        name: "Spring-Shower Elliott",
+        level: 42,
+        area: "Kingdom of Tantal (Lumos Pillar)",
+        time: "Anytime",
+        uid: "um_springShowerElliott"
+    },
+    {
+        name: "Muscley Damian",
+        level: 43,
+        area: "Kingdom of Tantal (Sanguinous Grotto)",
+        time: "Anytime",
+        uid: "um_muscleyDamian"
+    },
+    {
+        name: "Heroic Edwin",
+        level: 44,
+        area: "Kingdom of Tantal (Pillar City Ruins)",
+        time: "Anytime",
+        uid: "um_heroicEdwin"
+    },
+    {
+        name: "Climactic Honnold",
+        level: 45,
+        area: "Leftherian Archipelago (Isle of Urchon)",
+        time: "Anytime",
+        uid: "um_climacticHonnold"
+    },
+    {
+        name: "Venal Montgomery",
+        level: 45,
+        area: "Gormott Province (Coolley Lake)",
+        time: "Anytime",
+        uid: "um_venalMontgomery"
+    },
+    {
+        name: "Autumn-Shower Melvin",
+        level: 46,
+        area: "Kingdom of Tantal (Lumos Pillar)",
+        time: "Nighttime after Spring-Shower Elliott has been defeated",
+        uid: "um_autumnShowerMelvin"
+    },
+    {
+        name: "Excavator Darius",
+        level: 46,
+        area: "Spirit Crucible Elpys (Underground River Lavi)",
+        time: "Anytime",
+        uid: "um_excavatorDarius"
+    },
+    {
+        name: "Spellbinder Billy",
+        level: 46,
+        area: "Kingdom of Tantal (Genbu Drifts )",
+        time: "Snow",
+        uid: "um_spellbinderBilly"
+    },
+    {
+        name: "Perplexed Stoyan",
+        level: 47,
+        area: "Leftherian Archipelago (Little Garden of Tradition)",
+        time: "Anytime",
+        uid: "um_perplexedStoyan"
+    },
+    {
+        name: "Vile Howard",
+        level: 47,
+        area: "Spirit Crucible Elpys (Wonder Spring Plaza Ruins)",
+        time: "Anytime",
+        uid: "um_vileHoward"
+    },
+    {
+        name: "Epicurean Ligia",
+        level: 48,
+        area: "Leftherian Archipelago (Lud Cloudway)",
+        time: "Anytime",
+        uid: "um_epicureanLigia"
+    },
+    {
+        name: "Hard-Bitten Xavier",
+        level: 48,
+        area: "Spirit Crucible Elpys (Elpys Great Hall)",
+        time: "Anytime",
+        uid: "um_hardBittenXavier"
+    },
+    {
+        name: "Howitzer Leon",
+        level: 48,
+        area: "Kingdom of Uraya (Old Quarry)",
+        time: "Anytime",
+        uid: "um_howitzerLeon"
+    },
+    {
+        name: "Rapturous Scandia",
+        level: 48,
+        area: "Leftherian Archipelago (Godsford Isle)",
+        time: "Anytime",
+        uid: "um_rapturousScandia"
+    },
+    {
+        name: "Peerless Beaufort",
+        level: 49,
+        area: "Leftherian Archipelago (Isle of Sleeping Remains)",
+        time: "Anytime",
+        uid: "um_peerlessBeaufort"
+    },
+    {
+        name: "Glamorous Alfred",
+        level: 50,
+        area: "Spirit Crucible Elpys (Chamber of Offering)",
+        time: "Anytime",
+        uid: "um_glamorousAlfred"
+    },
 ]
+/*
+{
+    name: "",
+    level: ,
+    area: "()",
+    time: "",
+    uid: "um_"
+},
+*/
+
+/*
+    
+    {
+        name: "Antecedent Xiaxia",
+        level: 99,
+        area: "Empire of Mor Ardain (Old Factory)",
+        time: "After entering the First Low Orbit Station",
+        uid: "um_antecedentXiaxia"
+    },
+    {
+        name: "Armored Brennan",
+        level: 86,
+        area: "Empire of Mor Ardain (Chansagh Wastes)",
+        time: "Anytime",
+        uid: "um_armoredBrennan"
+    },
+    {
+        name: "Artifice Ophion",
+        level: 117,
+        area: " ()",
+        time: "",
+        uid: "um_"
+    },
+*/
