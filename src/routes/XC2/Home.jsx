94fdcc2c2ae17c74c8c26ed9b6d3776e3beb93a7
Griffin Thaxter <thaxterg12@gmail.com>
2024-10-01 10:42:32 -0600

    XC2 Home page done

--- src/routes/XC2/Home.jsx
+++ src/routes/XC2/Home.jsx
@@ -1,10 +1,18 @@
-import NavBar from "./Navbar";
+import { Link } from 'react-router-dom';
+
+import NavBar from './Navbar';
 
 export default function XCTwoHome() {
     return (
         <>
             <NavBar />
-            <h1>Xenobalde 2</h1>
+            
+            <br />
+            <div className="m-16 w-screen">
+                <h1 className="text-2xl font-bold">Xenoblade Chronicles 2 Completetion Checklist</h1>
+                <p>Hi there this is the completion checklist for all things Xenoblade Chronicles 2</p>
+                <h1 className="text-xl font-bold italic underline text-red-800">THIS IS NOT A SPOILER FREE LIST A SPOILER FREE VERSION IS COMING SOON</h1>
+            </div>
         </>
-    );
+    )
 }
