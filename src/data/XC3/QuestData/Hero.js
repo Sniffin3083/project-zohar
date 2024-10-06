dc3d0da9eb118c33cd2faaa86c99bfef4d661439
Griffin Thaxter <thaxterg12@gmail.com>
2024-10-06 15:30:19 -0600

    All hero quests added

--- src/data/XC3/QuestData/Hero.js
+++ src/data/XC3/QuestData/Hero.js
@@ -1,5 +1,371 @@
 export default [
     {
-        
-    }
+        name: "Going Beyond Power",
+        giver: "Kyrie",
+        prerequisites: "No Want of Courage quest complete",
+        chapter: "After leaving Colony 4",
+        reigon: "Aetia Region",
+        location: "Alfeto Valley",
+        uid: "heroQ_goingBeyondPower"
+    },
+    {
+        name: "A Gray Matter",
+        giver: "None",
+        prerequisites: "The Kind Right Hand quest complete",
+        chapter: "After leaving Colony 4",
+        reigon: "Aetia Region",
+        location: "Millick Meadows",
+        uid: "heroQ_aGrayMatter"
+    },
+    {
+        name: "Reasons to Evolve",
+        giver: "Kite",
+        prerequisites: "For Colony 9 quest completed | Zeon in the party | Guardian Commander rank 10",
+        chapter: "Chapter 5",
+        reigon: "Aetia Region",
+        location: "Yzana Plains",
+        uid: "heroQ_reasonsToEvolve"
+    },
+    {
+        name: "My Memories",
+        giver: "Triton",
+        prerequisites: "Manana's Battle Soup cooked | Soulhacker Rank 10 reached | \"Manana's Cooking\" discussed | Triton in the party",
+        chapter: "Chapter 5",
+        reigon: "Aetia Region",
+        location: "Millick Meadows",
+        uid: "heroQ_myMemories"
+    },
+    {
+        name: "Side Story: Mio",
+        giver: "Mio",
+        prerequisites: "None",
+        chapter: "Chapter 6",
+        reigon: "Aetia Region",
+        location: "Captocorn Peak",
+        uid: "heroQ_sideStoryMio"
+    },
+    {
+        name: "A Twist of Fate",
+        giver: "Cammuravi",
+        prerequisites: "Side Story: Mio completed",
+        chapter: "Chapter 6",
+        reigon: "Aetia Region",
+        location: "Captocorn Peak",
+        uid: "heroQ_aTwistOfFate"
+    },
+    {
+        name: "Shadow of Enmity",
+        giver: "Teach",
+        prerequisites: "Extracurricular Lesson quest completed | Teach in the party | Thaumaturge Rank 10 reached",
+        chapter: "Chapter 6",
+        reigon: "Aetia Region",
+        location: "Melnath's Shoulder",
+        uid: "heroQ_shadowOfEnmity"
+    },
+    {
+        name: "Inhumanity",
+        giver: "None",
+        prerequisites: "The Illusion Returns quest completed",
+        chapter: "Chapter 6",
+        reigon: "Aetia Region",
+        location: "Captocorn Peak",
+        uid: "heroQ_inhumanity"
+    },
+    {
+        name: "Grasping My Future",
+        giver: "Nia",
+        prerequisites: "Nia's Memories discussed | Lifesage rank 10 | Nia in the party",
+        chapter: "After Completing The Main Story",
+        reigon: "Aetia Region",
+        location: "Captocorn Peak",
+        uid: "heroQ_graspingMyFuture"
+    },
+    {
+        name: "Champion of the Nopon (DLC)",
+        giver: "None",
+        prerequisites: "None",
+        chapter: "From the Beginning",
+        reigon: "Fornis Region",
+        location: "Expansion Pass",
+        uid: "heroQ_championOfTheNopon"
+    },
+    {
+        name: "No Want of Courage",
+        giver: "Taion",
+        prerequisites: "Helping Out quest started | \"Colony 4's Problems\" discussed",
+        chapter: "Chapter 3",
+        reigon: "Fornis Region",
+        location: "Eagus Wilderness",
+        uid: "heroQ_noWantOfCourage"
+    },
+    {
+        name: "The Kind Right Hand",
+        giver: "Valdi",
+        prerequisites: "None",
+        chapter: "Chapter 3",
+        reigon: "Fornis Region",
+        location: "Ribbi Flats",
+        uid: "heroQ_theKindRightHand"
+    },
+    {
+        name: "Where The Heart Is",
+        giver: "Colony 4 Soldier",
+        prerequisites: "Colony 9 Lately discussed",
+        chapter: "Chapter 3",
+        reigon: "Fornis Region",
+        location: "Eagus Wilderness",
+        uid: "heroQ_whereTheHeartIs"
+    },
+    {
+        name: "Her Reasons",
+        giver: "Alexandria",
+        prerequisites: "None",
+        chapter: "Chapter 3",
+        reigon: "Fornis Region",
+        location: "Elaice Highway",
+        uid: "sideQ_herReasons"
+    },
+    {
+        name: "I'm a Mechanic",
+        giver: "Yuzet",
+        prerequisites: "Dorrick project at Level 5 | Boundary unlocked | War Medic Rank 10 | Valdi in the party",
+        chapter: "After reaching Erythia Sea",
+        reigon: "Fornis Region",
+        location: "Ribbi Flats",
+        uid: "heroQ_imAMechanic"
+    },
+    {
+        name: "The Hope of Noponkind (DLC)",
+        giver: "Sunny",
+        prerequisites: "Going Full HD quest completed | The Castle Beckons quest accepted | Noponic Champion Rank 10 | Ino in the party",
+        chapter: "After reaching Erythia Sea",
+        reigon: "Fornis Region",
+        location: "Dannagh Desert",
+        uid: "heroQ_theHopeOfNoponkind"
+    },
+    {
+        name: "Inescapable Past",
+        giver: "Gambert",
+        prerequisites: "Scant Supplies quest completed | Incursor Rank 10 reached | Alexandria in the party",
+        chapter: "After reaching Erythia Sea",
+        reigon: "Fornis Region",
+        location: "Elaice Highway",
+        uid: "heroQ_inescapablePast"
+    },
+    {
+        name: "A Deep-Seated Scar",
+        giver: "Ashera",
+        prerequisites: "Three Ravens at War quest completed | Side Story: Noah quest started | Lone Exile Rank 10 | Ashera in the party",
+        chapter: "Chapter 7",
+        reigon: "Fornis Region",
+        location: "Rae-Bel Tableland",
+        uid: "heroQ_aDeepSeatedScar"
+    },
+    {
+        name: "A Nopon's Counsel",
+        giver: "Riku & Manana",
+        prerequisites: "None",
+        chapter: "After reaching the Pentelas Region",
+        reigon: "Pentelas Region",
+        location: "Urayan Tunnels",
+        uid: "heroQ_aNoponsCounsel"
+    },
+    {
+        name: "Unwavering Resolve",
+        giver: "Isurd",
+        prerequisites: "None",
+        chapter: "Chapter 4",
+        reigon: "Pentelas Region",
+        location: "Great Cotte Falls",
+        uid: "heroQ_unwaveringResolve"
+    },
+    {
+        name: "Natural Selection",
+        giver: "None",
+        prerequisites: "None",
+        chapter: "Chapter 4",
+        reigon: "Pentelas Region",
+        location: "High Maktha Wildwood",
+        uid: "heroQ_naturalSelection"
+    },
+    {
+        name: "Survivors",
+        giver: "Consul U",
+        prerequisites: "Learning from Lambda quest completed | Stalker Rank 10 | Juniper in the party",
+        chapter: "Chapter 6",
+        reigon: "Pentelas Region",
+        location: "High Maktha Wildwood",
+        uid: "heroQ_survivors"
+    },
+    {
+        name: "Effervescent Heart",
+        giver: "Isurd",
+        prerequisites: "New Developments quest completed | Strategos rank 10 reached | Isurd in the party",
+        chapter: "Chapter 6",
+        reigon: "Pentelas Region",
+        location: "Great Cotte Falls",
+        uid: "heroQ_effervescentHeart"
+    },
+    {
+        name: "The Wrath of Ashera",
+        giver: "Ashera",
+        prerequisites: "None",
+        chapter: "After reaching the Keves Castle Region",
+        reigon: "Keves Castle Region",
+        location: "Syra Hovering Reefs",
+        uid: "heroQ_theWrathOfAshera"
+    },
+    {
+        name: "Side Story: Noah",
+        giver: "Samon",
+        prerequisites: "The Ultimate Vessel quest accepted | 5 Origin Metals obtained",
+        chapter: "Chapter 7",
+        reigon: "Keves Castle Region",
+        location: "Elaice Highway or Low Maktha Wildwood",
+        uid: "heroQ_sideStoryNoah"
+    },
+    {
+        name: "From Atop Her Throne",
+        giver: "Aizel",
+        prerequisites: "All Colonies liberated | Royal Summoner rank 10 | Melia in the party",
+        chapter: "After Completeing the Main Story",
+        reigon: "Keves Castle Region",
+        location: "Keves Castle",
+        uid: "heroQ_fromAtopHerThrone"
+    },
+    {
+        name: "Transparent Dreams",
+        giver: "Fiona",
+        prerequisites: "None",
+        chapter: "After reaching Erythia Sea",
+        reigon: "Cadensia Region",
+        location: "Erythia Sea",
+        uid: "heroQ_transparentDreams"
+    },
+    {
+        name: "Doing It My Way",
+        giver: "Triton",
+        prerequisites: "Ishan Isle visited",
+        chapter: "After reaching Erythia Sea",
+        reigon: "Cadensia Region",
+        location: "Erythia Sea",
+        uid: "heroQ_doingItMyWay"
+    },
+    {
+        name: "Culinary Repertoire",
+        giver: "Riku and Manana",
+        prerequisites: "Recipe for Cerise-Caprice Acqua Pazza learned | Yumsmith Rank 10 | Riku & Manana in the party",
+        chapter: "Chapter 6",
+        reigon: "Cadensia Region",
+        location: "Erythia Sea",
+        uid: "heroQ_culinaryRepertoire"
+    },
+    {
+        name: "Side Story: Taion",
+        giver: "Isurd",
+        prerequisites: "The Sea discussed | Riku & Manana in the party",
+        chapter: "Chapter 6",
+        reigon: "Cadensia Region",
+        location: "Erythia Sea",
+        uid: "heroQ_sideStoryTaion"
+    },
+    {
+        name: "A Farewell Reset",
+        giver: "Fiona",
+        prerequisites: "To Face Forward quest completed | Signifer rank 10 | Fiona in the party",
+        chapter: "Chapter 6",
+        reigon: "Cadensia Region",
+        location: "Erythia Sea",
+        uid: "heroQ_aFarewellReset"
+    },
+    {
+        name: "Invisible Bond",
+        giver: "Segiri",
+        prerequisites: "Acknowledging Feelings quest complete | Machine Assassin rank 10 | Segiri in party",
+        chapter: "Chapter 7",
+        reigon: "Cadensia Region",
+        location: "Li Garte Prison Camp",
+        uid: "heroQ_invisibleBond"
+    },
+    {
+        name: "Lapidarist Extraordinaire (DLC)",
+        giver: "None",
+        prerequisites: "None",
+        chapter: "Available from the start",
+        reigon: "City",
+        location: "Expansion Pass",
+        uid: "heroQ_lapidaristExtraordinaire"
+    },
+    {
+        name: "Vandham's Heir",
+        giver: "City Resident",
+        prerequisites: "Castle Access quest accepted | \"Guernica Vandham\" discussed",
+        chapter: "After reaching the City",
+        reigon: "City",
+        location: "Praetorian Hill",
+        uid: "heroQ_vandhamsHeir"
+    },
+    {
+        name: "Side Story: Eunie",
+        giver: "Bolearis",
+        prerequisites: "Castle Access quest accepted | Fourtune Clovers discussed | Riku & Manana in the party",
+        chapter: "After reaching the City",
+        reigon: "City",
+        location: "City Camp",
+        uid: "heroQ_sideStoryEunie"
+    },
+    {
+        name: "Payback for Treason",
+        giver: "Rozana",
+        prerequisites: "Lovebirds quest complete | Full Metal Jaguar Rank 10 | Gray in the party",
+        chapter: "After reaching Erythia Sea",
+        reigon: "City",
+        location: "Caelum Residential Quarter",
+        uid: "heroQ_paybackForTreason"
+    },
+    {
+        name: "For the Sake of Keepsakes (DLC)",
+        giver: "Masha",
+        prerequisites: "Lapidarist Rank 10 | Masha in the party",
+        chapter: "After leaving for the Cloudkeep",
+        reigon: "City",
+        location: "City Camp",
+        uid: "heroQ_forTheSakeOfKeepsakes"
+    },
+    {
+        name: "Happiness",
+        giver: "Miyabi",
+        prerequisites: "Troubadour Rank 10 reached | Miyabi in the party",
+        chapter: "After leaving for the Cloudkeep",
+        reigon: "City",
+        location: "Michiba Canteen",
+        uid: "heroQ_happiness"
+    },
+    {
+        name: "Side Story: Sena",
+        giver: "Monica",
+        prerequisites: "Samon talked to after meeting with Nia | Ghondor's Grumblings discussed | Ghondor in the party",
+        chapter: "Chapter 7",
+        reigon: "City",
+        location: "Mxene Tech Quarter",
+        uid: "heroQ_sideStorySena"
+    },
+    {
+        name: "Promise to the Future",
+        giver: "Monica",
+        prerequisites: "Lost Vanguard Rank 10 reached | Monica in the party",
+        chapter: "Chapter 7",
+        reigon: "City",
+        location: "Virid Park",
+        uid: "heroQ_promiseToTheFuture"
+    },
+    {
+        name: "Knowing Your Family",
+        giver: "Ghondor",
+        prerequisites: "Martial Artist Rank 10 reached | Ghondor in the party",
+        chapter: "Chapter 7",
+        reigon: "Agnus Castle Region",
+        location: "Ascension Grounds",
+        uid: "heroQ_knowingYourFamily"
+    },
 ]
