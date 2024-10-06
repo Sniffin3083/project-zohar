bf653db3a86399fd36c3f68f727cd028200a2fc6
Griffin Thaxter <thaxterg12@gmail.com>
2024-10-06 15:30:07 -0600

    All unmarked quests added

--- src/data/XC3/QuestData/Unmarked.js
+++ src/data/XC3/QuestData/Unmarked.js
@@ -1,5 +1,26 @@
 export default [
     {
-        
-    }
+        name: "Raising Armu",
+        prerequisites: "After The Future I Want to Spin",
+        chapter: "Chapter 6",
+        reigon: "Cadensia Region",
+        location: "Colony Mu",
+        uid: "unmarkedQ_raisingArmu"
+    },
+    {
+        name: "Ethel Maturity",
+        prerequisites: "Cammuravi in party",
+        chapter: "Chapter 6",
+        reigon: "Aetia Region",
+        location: "Colony Omega",
+        uid: "unmarkedQ_ethelMaturity"
+    },
+    {
+        name: "Blade Upgrade",
+        prerequisites: "After Uniting the Seven Nopon",
+        chapter: "Chapter 7",
+        reigon: "City",
+        location: "City",
+        uid: "unmarkedQ_bladeUpgrade"
+    },
 ]
