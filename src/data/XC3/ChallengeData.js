8937003d5aa1e06f47e29af96b82f7fad955a0d7
Griffin Thaxter <thaxterg12@gmail.com>
2024-10-08 20:12:50 -0600

    All challenge data added

--- src/data/XC3/ChallengeData.js
+++ src/data/XC3/ChallengeData.js
@@ -1,5 +1,141 @@
+import oneStar from "../../images/XC3/challengeModeDifficulty/1star.webp"
+import twoStar from "../../images/XC3/challengeModeDifficulty/2star.webp"
+import threeStar from "../../images/XC3/challengeModeDifficulty/3star.webp"
+import fourStar from "../../images/XC3/challengeModeDifficulty/4star.webp"
+import fiveStar from "../../images/XC3/challengeModeDifficulty/5star.webp"
+
 export default [
     {
-        
-    }
+        name: "Defenders' Gut Check",
+        diff: <img src={oneStar} alt="1 Star" />,
+        wave: 3,
+        level: 20,
+        uid: "chall_defendersGutCheck"
+    },
+    {
+        name: "Insecticide",
+        diff: <img src={oneStar} alt="1 Star" />,
+        wave: 3,
+        level: 20,
+        uid: "chall_insecticide"
+    },
+    {
+        name: "Philharmonic Dust-Up",
+        diff: <img src={twoStar} alt="2 Star" />,
+        wave: 3,
+        level: 20,
+        uid: "chall_philharmonicDustUp"
+    },
+    {
+        name: "Six Angry Beasts",
+        diff: <img src={threeStar} alt="3 Star" />,
+        wave: 3,
+        level: 30,
+        uid: "chall_sixAngryBeasts"
+    },
+    {
+        name: "Vampiric Rush",
+        diff: <img src={threeStar} alt="3 Star" />,
+        wave: 3,
+        level: 40,
+        uid: "chall_vampiricRush"
+    },
+    {
+        name: "Black Castle Chosen",
+        diff: <img src={twoStar} alt="2 Star" />,
+        wave: 3,
+        level: 40,
+        uid: "chall_blackCastleChosen"
+    },
+    {
+        name: "Ocean Day Parade",
+        diff: <img src={twoStar} alt="2 Star" />,
+        wave: 5,
+        level: 50,
+        uid: "chall_oceanDayParade"
+    },
+    {
+        name: "Pretty Fierce",
+        diff: <img src={threeStar} alt="3 Star" />,
+        wave: 4,
+        level: 50,
+        uid: "chall_prettyFierce"
+    },
+    {
+        name: "Ouroboros Fever",
+        diff: <img src={twoStar} alt="2 Star" />,
+        wave: 4,
+        level: 60,
+        uid: "chall_ouroborosFever"
+    },
+    {
+        name: "Ferron x Ferron",
+        diff: <img src={threeStar} alt="3 Star" />,
+        wave: 1,
+        level: 60,
+        uid: "chall_ferronXFerron"
+    },
+    {
+        name: "Double Jeopardy",
+        diff: <img src={fourStar} alt="4 Star" />,
+        wave: 1,
+        level: 70,
+        uid: "chall_doubleJeopardy"
+    },
+    {
+        name: "Arts and Crafts",
+        diff: <img src={threeStar} alt="3 Star" />,
+        wave: 5,
+        level: 70,
+        uid: "chall_artsAndCrafts"
+    },
+    {
+        name: "Stage of Destiny",
+        diff: <img src={fourStar} alt="4 Star" />,
+        wave: 4,
+        level: 70,
+        uid: "chall_stageOfDestiny"
+    },
+    {
+        name: "Men! Of! The Seal IV",
+        diff: <img src={fourStar} alt="4 Star" />,
+        wave: 4,
+        level: 70,
+        uid: "chall_menOfTheSealIV"
+    },
+    {
+        name: "N and M",
+        diff: <img src={fiveStar} alt="5 Star" />,
+        wave: 1,
+        level: 99,
+        uid: "chall_nAndM"
+    },
+    {
+        name: "Primate Triumvrate",
+        diff: <img src={fiveStar} alt="5 Star" />,
+        wave: 3,
+        level: 99,
+        uid: "chall_primateTriumvirate"
+    },
+    {
+        name: "Heroes Frontline",
+        diff: <img src={fiveStar} alt="5 Star" />,
+        wave: 3,
+        level: 99,
+        uid: "chall_heroesFrontline"
+    },
+    {
+        name: "The Four Devas",
+        diff: <img src={fiveStar} alt="5 Star" />,
+        wave: 4,
+        level: 99,
+        uid: "chall_theFourDevas"
+    },
+    {
+        name: "The Two Saviors",
+        diff: <img src={fourStar} alt="4 Star" />,
+        wave: 4,
+        level: 70,
+        uid: "chall_theTwoSaviors"
+    },
 ]
