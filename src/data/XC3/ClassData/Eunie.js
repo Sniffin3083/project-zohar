18f3e1fa75681ea2687cf578b26263ab6e1be819
Griffin Thaxter <thaxterg12@gmail.com>
2024-10-09 10:49:09 -0600

    All class Data added

--- src/data/XC3/ClassData/Eunie.js
+++ src/data/XC3/ClassData/Eunie.js
@@ -1,5 +1,209 @@
+import attacker from "../../../images/XC3/roleIcons/attacker_role.webp"
+import defender from "../../../images/XC3/roleIcons/defender_role.webp"
+import healer from "../../../images/XC3/roleIcons/healer_role.webp"
+
 export default [
     {
-        
-    }
+        name: "Swordfighter",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "Initial",
+        ascenQuest: "Side Story: Noah",
+        uid: "classEunie_swordfighter"
+    },
+    {
+        name: "Zepher",
+        role: <img src={defender} alt="Defender" />,
+        acquiQuest: "Initial",
+        ascenQuest: "Side Story: Mio",
+        uid: "classEunie_zepher"
+    },
+    {
+        name: "Medic Gunner",
+        role: <img src={healer} alt="Healer" />,
+        acquiQuest: "Initial",
+        ascenQuest: "Side Story: Eunie",
+        uid: "classEunie_medicGunner"
+    },
+    {
+        name: "Tactician",
+        role: <img src={healer} alt="Healer" />,
+        acquiQuest: "Initial",
+        ascenQuest: "Side Story: Taion",
+        uid: "classEunie_tactician"
+    },
+    {
+        name: "Heavy Guard",
+        role: <img src={defender} alt="Defender" />,
+        acquiQuest: "Initial",
+        ascenQuest: "Side Story: Lanz",
+        uid: "classEunie_heavyGuard"
+    },
+    {
+        name: "Ogre",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "Initial",
+        ascenQuest: "Side Story: Sena",
+        uid: "classEunie_ogre"
+    },
+    {
+        name: "Flash Fencer",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "No Want of Courage",
+        ascenQuest: "Reach Rank 10 with Flash Fencer and Seraph, and have Ethel undergo the maturation process at Colony Omega",
+        uid: "classEunie_flashFencer"
+    },
+    {
+        name: "War Medic",
+        role: <img src={healer} alt="Healer" />,
+        acquiQuest: "The Kind Right Hand",
+        ascenQuest: "I'm a Mechanic",
+        uid: "classEunie_warMedic"
+    },
+    {
+        name: "Guardian Commander",
+        role: <img src={defender} alt="Defender" />,
+        acquiQuest: "Where The Heart Is",
+        ascenQuest: "Reasons to Evolve",
+        uid: "classEunie_guardianCommander"
+    },
+    {
+        name: "Thaumaturge",
+        role: <img src={healer} alt="Healer" />,
+        acquiQuest: "Going Beyond Power",
+        ascenQuest: "Shadow of Enmity",
+        uid: "classEunie_thaumaturge"
+    },
+    {
+        name: "Yumsmith",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "A Nopon's Counsel",
+        ascenQuest: "Culinary Repertoire",
+        uid: "classEunie_yumsmith"
+    },
+    {
+        name: "Full Metal Jaguar",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "A Gray Matter",
+        ascenQuest: "Payback for Treason",
+        uid: "classEunie_fullMetalJaguar"
+    },
+    {
+        name: "Strategos",
+        role: <img src={healer} alt="Healer" />,
+        acquiQuest: "Unwavering Resolve",
+        ascenQuest: "Effervescent Heart",
+        uid: "classEunie_strategos"
+    },
+    {
+        name: "Stalker",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "Natural Selection",
+        ascenQuest: "Survivors",
+        uid: "classEunie_stalker"
+    },
+    {
+        name: "Lone Exile",
+        role: <img src={defender} alt="Defender" />,
+        acquiQuest: "The Wrath of Ashera",
+        ascenQuest: "A Deep-Seated Scar",
+        uid: "classEunie_loneExile"
+    },
+    {
+        name: "Incursor",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "Her Reasons",
+        ascenQuest: "Inescapable Past",
+        uid: "classEunie_incursor"
+    },
+    {
+        name: "Lost Vanguard",
+        role: <img src={defender} alt="Defender" />,
+        acquiQuest: "Vandham's Heir",
+        ascenQuest: "Promise to the Future",
+        uid: "classEunie_lostVanguard"
+    },
+    {
+        name: "Signifer",
+        role: <img src={healer} alt="Healer" />,
+        acquiQuest: "Transparent Dreams",
+        ascenQuest: "A Farewell Reset",
+        uid: "classEunie_signifer"
+    },
+    {
+        name: "Soul Hacker",
+        role: "Any",
+        acquiQuest: "Doing It My Way",
+        ascenQuest: "My Memories",
+        uid: "classEunie_soulHacker"
+    },
+    {
+        name: "Martial Artist",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "Chapter 6",
+        ascenQuest: "Knowing Your Family",
+        uid: "classEunie_martialArtist"
+    },
+    {
+        name: "Troubadour",
+        role: <img src={healer} alt="Healer" />,
+        acquiQuest: "Side Story: Mio",
+        ascenQuest: "Happiness",
+        uid: "classEunie_troubadour"
+    },
+    {
+        name: "Seraph",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "A Twist of Fate",
+        ascenQuest: "Reach Rank 10 with both the Flash Fencer and Seraph classes, and have Ethel undergo the maturation process at Colony Omega",
+        uid: "classEunie_seraph"
+    },
+    {
+        name: "Machine Assassin",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "Inhumanity",
+        ascenQuest: "Invisible Bond",
+        uid: "classEunie_machineAssassin"
+    },
+    {
+        name: "Lifesage",
+        role: <img src={healer} alt="Healer" />,
+        acquiQuest: "Automatically obtained after going to the Hall of the Serene in the Cloudkeep after completing the main story",
+        ascenQuest: "Grasping My Future",
+        uid: "classEunie_lifesage"
+    },
+    {
+        name: "Royal Summoner",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "Automatically obtained after going to the Main Castle Gate of Keves Castle after completing the main story",
+        ascenQuest: "From Atop Her Throne",
+        uid: "classEunie_royalSummoner"
+    },
+    {
+        name: "Noponic Champion (DLC)",
+        role: <img src={defender} alt="Defender" />,
+        acquiQuest: "Champion of the Nopon",
+        ascenQuest: "The Hope of Noponkind",
+        uid: "classEunie_noponicChampion"
+    },
+    {
+        name: "Lapidarist (DLC)",
+        role: <img src={healer} alt="Healer" />,
+        acquiQuest: "Lapidarist Extraordinaire",
+        ascenQuest: "For the Sake of Keepsakes",
+        uid: "classEunie_lapidarist"
+    },
+    {
+        name: "Grand Marshal (DLC)",
+        role: <img src={defender} alt="Defender" />,
+        acquiQuest: "Clear Challenge Battle: The Two Saviors",
+        ascenQuest: "None",
+        uid: "classEunie_grandMarshal"
+    },
+    {
+        name: "Master Driver (DLC)",
+        role: <img src={attacker} alt="Attacker" />,
+        acquiQuest: "Clear Challenge Battle: The Two Saviors",
+        ascenQuest: "None",
+        uid: "classEunie_masterDriver"
+    },
 ]
